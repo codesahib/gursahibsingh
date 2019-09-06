fc4ec9296e0a280a6da464d7ee483a13363f9f64
codesahib <gursahib.kvm@gmail.com>
2019-09-06 09:28:52 +0530

    [MAJOR UPDATE] 6/9/2019: Removed Carousel and instead added bg img. Changed Website & Navbar Theme. Removed 'Project' Submenu, instead created landing page for project and then redirection

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	path('', views.projects, name='projects'),
     path(r'ml', views.project_ml, name='project-ml'),
     path(r'android', views.project_android, name='project-android'),
     path(r'web', views.project_web, name='project-web'),
--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+def projects(request):
+    return render(request, 'projects-page/projects.html')
+
 def project_ml(request):
     return render(request, 'projects-page/projects-ml.html')
 
