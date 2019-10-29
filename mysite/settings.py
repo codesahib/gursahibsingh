9ca52b2c5625b2557583980426e282d3d878322f
codesahib <gursahib.kvm@gmail.com>
2019-10-30 01:23:05 +0530

    Updated on 30/10/2019

--- mysite/settings.py
+++ mysite/settings.py
@@ -138,5 +138,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')  #F:\Projects\Website\mysite\static #Production mode,colelctstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  #F:\Projects\Website\mysite\static #Production mode,collectstatic
 STATIC_URL = '/static/'
