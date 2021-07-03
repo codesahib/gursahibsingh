50dbb3871420d8737c626dfba2128c5214d4c514
codesahib <gursahib.kvm@gmail.com>
2021-07-03 20:18:00 +0530

    [2021-07-03] Changed dir structure according to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer';
+import Navbar from './components/common/Navbar'
+import Footer from './components/common/Footer';
 
 import './App.css';
 
