5e9aa6a5c1b2d7dde24d676dd98574f6a4f0b779
codesahib <gursahib.kvm@gmail.com>
2021-07-04 19:58:57 +0530

    [2021-07-04] Started working on Project Component

--- frontend/src/components/projects/Projects.js
+++ frontend/src/components/projects/Projects.js
@@ -3,8 +3,14 @@ import React from 'react'
 export default function Projects() {
     document.title = "Projects | Gursahib Singh"
     return (
-        <div>
-            Projects
+        <div id="projectComponent">
+            <div className="section" style={{backgroundColor: "#4d4855"}}>
+                <h1 className="content-heading">PROJECTS</h1>     
+                <div className="section-part-large" style={{backgroundColor: "#FCFCF5"}}>
+                <p class="content-para text-dark"> I've tried doing projects in many seemingly intereseting fields. The range of areas that I've worked upon are <span className="text-highlight">Web Development, Machine Learning, App Development(very basic), and Scripting.</span></p>
+                <p class="content-para text-dark"> I plan to work more on the <span className="text-highlight">Cloud and Automation</span> tools that will eventually help stand in sync with the changing world</p>
+                </div>
+            </div>
         </div>
     )
 }
