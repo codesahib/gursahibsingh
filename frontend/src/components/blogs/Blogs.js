9a6f59c9ac85b33c5cb9826e2633bba050abcff6
codesahib <gursahib.kvm@gmail.com>
2021-07-05 01:32:16 +0530

    [2021-07-05] Started with Blogs component

--- frontend/src/components/blogs/Blogs.js
+++ frontend/src/components/blogs/Blogs.js
@@ -1,10 +1,32 @@
 import React from 'react'
+import { Card, Button } from 'react-bootstrap' 
+
+import Thailand from '../../static/img/thailand.jpg'
+
+import './Blogs.css'
 
 export default function Blogs() {
     document.title = "Blogs | Gursahib Singh"
     return (
-        <div>
-            Blogs
+        <div id="blogsComponent">
+            <div id="blogsContainer">
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={Thailand} />
+                <Card.Body>
+                    <Card.Title>Thailand -2019</Card.Title>
+                    <Card.Text>
+                    The complete experience and some useful tips.
+                    </Card.Text>
+                    <Button>Read</Button>
+                </Card.Body>
+            </Card>
+            </div>
+            <div id="otherLinksContainer">
+                <h1 className="content-heading">Other Blogs</h1>
+                <a className="content-para text-dark" href="https://udemyfreecoursehere.blogspot.com/">1. Udemy Free Courses</a>
+                <a className="content-para text-dark" href="https://sahibincanada.blogspot.com/">2. Canada Visa process</a>
+                <a className="content-para text-dark" href="https://sahibdroid.blogspot.com/">3. Android Tips and Tricks</a>
+            </div>
         </div>
     )
 }
