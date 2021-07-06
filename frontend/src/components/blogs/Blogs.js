a8a41b00823784b3ce959bcbe53e02f39cbf991c
codesahib <gursahib.kvm@gmail.com>
2021-07-07 00:36:45 +0530

    [2021-07-07] Renaming id of container having blog cards

--- frontend/src/components/blogs/Blogs.js
+++ frontend/src/components/blogs/Blogs.js
@@ -23,7 +23,7 @@ export default function Blogs(props) {
                 <p className="content-para text-dark">There are lot of appreciable things that happen around us and with us that we fail to notice. We need to sit back, relax and reflect upon those wonderful moments and writing down is the best way to do so. This section would contain information based and experience based blogs. No-nonsense!</p>
                 </div>
             </div>
-            <div id="blogsContainer">
+            <div id="blogCardsContainer">
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src={Thailand} />
                     <Card.Body>
