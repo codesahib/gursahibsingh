e42b853eb4e97d813ac7a97ed83f90c1e5810d19
codesahib <gursahib.kvm@gmail.com>
2021-07-06 21:06:45 +0530

    [2021-07-06] Adding a utility 'ScrollToTop' so that screen is at top when a component page loads up on Navbar link click.
    This is needed when redirecting through 'Route'

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ScrollToTop from "./components/utils/ScrollToTop";
 import reportWebVitals from './reportWebVitals';
 
 import {HashRouter} from 'react-router-dom'
@@ -9,6 +10,7 @@ import {HashRouter} from 'react-router-dom'
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>,
