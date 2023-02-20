cf0b063855fd63a4f122ccaf2bf81ca476be2c36
codesahib <codesahib@gmail.com>
2023-02-21 03:26:05 +0530

    Updating timeline, resume and footer

--- frontend/src/components/home/Timeline.js
+++ frontend/src/components/home/Timeline.js
@@ -5,36 +5,54 @@ import './Timeline.css'
 
 export default function Timeline() {
     const items = [
-      {
-            title: "2019-Present",
+        {
+            title: "2021-Present",
+            cardTitle: "Full time job",
+            cardSubtitle:"Full Stack Developer",
+            cardDetailedText: "BluekeyAI, Toronto"
+        },
+        {
+            title: "2022",
+            cardTitle: "Internship",
+            cardSubtitle:"Full Stack Developer, UbiLab",
+            cardDetailedText: "University of Waterloo"
+        },
+        {
+            title: "2021-2022",
+            cardTitle: "Master of Engineering",
+            cardSubtitle:"ECE (Diploma in Business and Entrepreneurship)",
+            cardDetailedText: "University of Waterloo"
+        },
+        {
+            title: "2019-2021",
             cardTitle: "Full time job",
             cardSubtitle:"Senior Member Technical Staff",
             cardDetailedText: "Siemens EDA(Mentor Graphics), Noida"
-      },
-      {
+        },
+        {
             title: "2019",
             cardTitle: "Internship",
             cardSubtitle:"Software Intern",
             cardDetailedText: "Siemens EDA(Mentor Graphics), Noida"
-      },
-      {
+        },
+        {
             title: "2015-2019",
             cardTitle: "Bachelor of Engineering",
             cardSubtitle:"CGPA: 9.50",
             cardDetailedText: "Thapar Institute of Engineering and Technology, Patiala"
-      },
-      {
-        title: "2015",
-        cardTitle: "Class 12th",
-        cardSubtitle:"95.6%",
-        cardDetailedText: "Kundan Vidya Mandir, Ludhiana"
-      },
-      {
-        title: "2013",
-        cardTitle: "Class 10th",
-        cardSubtitle:"CGPA: 10",
-        cardDetailedText: "Kundan Vidya Mandir, Ludhiana"
-      }
+        },
+        // {
+        //     title: "2015",
+        //     cardTitle: "Class 12th",
+        //     cardSubtitle:"95.6%",
+        //     cardDetailedText: "Kundan Vidya Mandir, Ludhiana"
+        // },
+        // {
+        //     title: "2013",
+        //     cardTitle: "Class 10th",
+        //     cardSubtitle:"CGPA: 10",
+        //     cardDetailedText: "Kundan Vidya Mandir, Ludhiana"
+        // }
     ];
 
     const theme={
