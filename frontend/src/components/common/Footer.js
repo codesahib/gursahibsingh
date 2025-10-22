e6b620cd7be324d98989f04001c79599ba063f58
Gursahib Singh <codesahib@gmail.com>
2025-10-22 00:58:06 -0400

    Footer changes

--- frontend/src/components/common/Footer.js
+++ frontend/src/components/common/Footer.js
@@ -1,35 +1,29 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import './Footer.css'
 
 export default function Footer() {
     return (
         <footer className="page-footer">
-            <div id="contactContainer" className="container text-center text-md-left">
-                <h1 id="textContact">CONTACT</h1>
-                <div id="linksContainer" className="container">
-                    <div id="emailContainer">
-                        <p><a className="fa fa-google" href="mailto:iamgursahib@gmail.com"></a>&nbsp;&nbsp;iamgursahib@gmail.com</p>
-                    </div>
-                    <div id="socialMediaContainer">
-                        <ul>
-                            <li>
-                                <a className="fa fa-linkedin" href="https://www.linkedin.com/in/gursahib-singh-04849b156/" target="_blank"></a>
-                            </li>
-                            <li>
-                                <a className="fa fa-github" href="https://github.com/codesahib"  target="_blank"></a>
-                            </li>
+            <div className="footer-main">
+                <h1 className="footer-heading">CONTACT</h1>
 
-                            <li>
-                                <a className="fa fa-instagram" href="https://www.instagram.com/gursahib__singh/?hl=en" target="_blank"></a>
-                            </li>
-                        </ul>
+                <div className="footer-links">
+                    <div className="footer-email">
+                        <a href="mailto:iamgursahib@gmail.com" className="fa fa-google"></a>
+                        <span> iamgursahib@gmail.com</span>
+                    </div>
+
+                    <div className="footer-social">
+                        <a className="fa fa-linkedin" href="https://www.linkedin.com/in/gursahib-singh-04849b156/" target="_blank" rel="noopener noreferrer"></a>
+                        <a className="fa fa-github" href="https://github.com/codesahib" target="_blank" rel="noopener noreferrer"></a>
+                        <a className="fa fa-instagram" href="https://www.instagram.com/gursahib__singh/?hl=en" target="_blank" rel="noopener noreferrer"></a>
                     </div>
                 </div>
             </div>
-        
-            <div className="footer-copyright text-center">
+
+            <div className="footer-bottom">
                 <p>© 2025 Copyright: <Link to="/">codesahib.github.io/gursahibsingh</Link></p>
             </div>
         </footer>
