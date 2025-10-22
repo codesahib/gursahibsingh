000136cc4891fda919836be65d2cd26033a0829f
Gursahib Singh <codesahib@gmail.com>
2025-10-22 01:26:22 -0400

    update projects

--- frontend/src/components/projects/Projects.js
+++ frontend/src/components/projects/Projects.js
@@ -5,47 +5,88 @@ import './Projects.css';
 // Example data structure: group projects by category
 const projectData = [
   {
-    category: "Machine Learning Research Projects",
+    category: "AI/ML",
     projects: [
-      {
-        title: "Optimal Cryptocurrency Portfolio Selection Using Genetic Algorithm",
-        subtitle: "(Machine Learning Research Project)",
-        motivation: "With so many Cryptocurrencies in the market, what to choose?",
-        objective: "To find the optimal crypto portfolio given various assets using Genetic Algorithm techniques such as Natural Selection, Crossover and Mutation.",
-        github: "https://github.com/codesahib/Optimal-crypto-portfolio.git",
-        demo: "https://youtu.be/hmrj1J-Pwkk"
-      }
+        {
+            title: "AI-Powered IoT Alert Analysis (PoC)",
+            subtitle: "Python, LangChain, LLMs, IoT Logs, Kibana",
+            motivation: "Manual log review is tedious and slow.",
+            objective: "Automatically summarize anomalies and provide root-cause explanations using LLMs.",
+        },
+        {
+            title: "Talk-to-My-CSV",
+            subtitle: "Streamlit, LangChain, Ollama, Chroma, HuggingFace",
+            motivation: "Make exploring CSV data fast and intuitive without writing code.",
+            objective: "Enable users to ask questions and get answers from CSVs using a local AI model."
+        },
+        {
+            "title": "Standardized Admission Data Parser - Corporate project",
+            "subtitle": "Full Stack / NLP / Cloud Automation",
+            "motivation": "Manual data entry is slow and error-prone.",
+            "objective": "To automate extraction of structured insights from unformatted admission data using lightweight NLP models and AWS Lambda.",
+        },
+        {
+            title: "Optimal Cryptocurrency Portfolio Selection Using Genetic Algorithm",
+            subtitle: "Python, NumPy, Pandas, Matplotlib",
+            motivation: "With so many Cryptocurrencies in the market, what to choose?",
+            objective: "To find the optimal crypto portfolio given various assets using Genetic Algorithm techniques such as Natural Selection, Crossover and Mutation.",
+            github: "https://github.com/codesahib/Optimal-crypto-portfolio.git",
+            demo: "https://youtu.be/hmrj1J-Pwkk"
+        }
+    ]
+  },
+  {
+    category: "Software Engineering",
+    projects: [
+        {
+            title: "Smart Lighting for Microsoft- Corporate project",
+            subtitle: "Python, MQTT, Docker",
+            motivation: "Streamline building lighting control across multiple locations.",
+            objective: "Develop a platform valued at $9M to process thousands of sensors for real-time lighting management.",
+        },
+        {
+            title: "IoT Middleware for Google - Corporate project",
+            subtitle: "Python, GCP, Java, MQTT, REST API",
+            motivation: "Make IoT data flow smoothly and efficiently.",
+            objective: "Build middleware to stream sensor data from Kafka to Google’s UDMI broker using GCP and Terraform.",
+        },
+        
     ]
   },
   {
     category: "Full Stack Web Apps",
     projects: [
-      {
-        title: "ShareMyGaddi - Carpool web application",
-        subtitle: "(Full Stack Web App)",
-        motivation: "Web Stack Development is tough? Why not try it :)",
-        objective: "To develop a full-stack carpooling web application using React.js, Node.js, MongoDB and including end-to-end testing using Jest and Cypress.io.",
-        github: "https://github.com/karancode-singh/ShareMyGaddi-carpooling"
-      },
-      {
-        title: "Portfolio Website",
-        subtitle: "(A Dynamic Resume)",
-        motivation: "Paper based or even digital single page Resumes are boring",
-        objective: "To create a dynamic and reactive web-based portfolio",
-        github: "https://github.com/codesahib/mysite",
-        link: "/home"
-      },
-      {
-        title: "Project-ToDo",
-        subtitle: "(React.js Single Page App)",
-        motivation: "There are todo list app available for a single tasks. But this web app will be a todo list to track projects. Each project can then have it's own set of tasks.",
-        objective: "To learn React.js library with a hands-on project",
-        github: "https://github.com/codesahib/Project-ToDo/tree/master/frontend",
-        link: "https://codesahib.github.io/Project-ToDo/"
-      }
+        {
+            "title": "Dynamic Pricing Engine for Petco",
+            "subtitle": "React, Node, PostgreSQL, Redux",
+            "motivation": "Real-time pricing is complex but crucial for competitive advantage.",
+            "objective": "To build a dynamic pricing engine that adjusts product prices in real-time based on competitors, discounts, and sales.",
+        },
+        {
+            title: "ShareMyGaddi - Carpool web application",
+            subtitle: "Node, React, Jest, REST API, Swagger",
+            motivation: "Web Stack Development is tough? Why not try it :)",
+            objective: "To develop a full-stack carpooling web application using React.js, Node.js, MongoDB and including end-to-end testing using Jest and Cypress.io.",
+            github: "https://github.com/karancode-singh/ShareMyGaddi-carpooling"
+        },
+        {
+            title: "Portfolio Website",
+            subtitle: "Django, React, REST API",
+            motivation: "Paper based or even digital single page Resumes are boring",
+            objective: "To create a dynamic and reactive web-based portfolio",
+            github: "https://github.com/codesahib/gursahibsingh",
+            link: "/home"
+        },
+        {
+            title: "Project-ToDo",
+            subtitle: "React, MongoDB",
+            motivation: "There are todo list app available for a single tasks. But this web app will be a todo list to track projects. Each project can then have it's own set of tasks.",
+            objective: "To learn React.js library with a hands-on project",
+            github: "https://github.com/codesahib/Project-ToDo/tree/master/frontend",
+            link: "https://codesahib.github.io/Project-ToDo/"
+        }
     ]
   }
-  // Add other categories and projects here
 ];
 
 export default function Projects(props) {
@@ -86,10 +127,19 @@ export default function Projects(props) {
                             Demo
                         </a>
                         )}
-                        {proj.link && (
-                        <Link className="btn btn-link" to={proj.link}>
-                            Link
-                        </Link>
+                       {
+                            proj.link && (proj.link.startsWith("https://") ? (
+                                <a  className="btn btn-link"
+                                    href={proj.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    Link
+                                </a>
+                            ) : (
+                                <Link className="btn btn-link" to={proj.link}>
+                                Link
+                                </Link>
+                            )
                         )}
                     </div>
                 </div>
